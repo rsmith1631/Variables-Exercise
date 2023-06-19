005230fe0ae11a46325830a220efe61f752918c7
rsmith1631 <rsmith1631@gmail.com>
2023-06-19 02:30:08 -0700

    Declaring and Initializing the variable and Added Console.WriteLine

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declaring and initializing the variable
+string name = "Frank";
+int age = 38;
+char middleInitial = 'J';
+bool dogOwner = true;
+double currentTemp = 77.9;
+decimal cashAmount = 29.9m;
+
+Console.WriteLine($"My friend {name} is {age}, he goes by his middle name {middleInitial}. It's {dogOwner} that he is a Dog owner." +
+    $"He walked his dog in {currentTemp} degree weather to the bank yesterday. All to take out his remaining balance of ${cashAmount}.");
